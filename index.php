57d8844cbab07a9a31595a4e0854ec28d2bb92f4
Santiago Pantoja <camilolp16@gmail.com>
2021-09-29 16:57:47 -0500

    operadores y demas en la clase

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+// Asignación 
+
+$name = "Johan"; // string
+$lastname = 'Legarda';  // string
+$age = 30; // number
+
+$jobis = [
+    'monday' => 'read',
+    'sunday' => 'play the guitar'
+]; 
+
+// echo $name; 
+echo 'Hola! Mi nombre es ' . $name . ' y tengo ' . $age . ' años <br>';
+
+// Operadores aritmeticos 
+
+// echo '2 + 3 = ' . (2+3);
+// echo '<br> 3 * 5 = ' . (3*5);
+
+$a = 30;
+$b = 15; 
+
+echo '30 + 15 = ' . ($a + $b);
+echo '<br> 30 * 15 = ' . ($a * $b);
+echo '<br> 30 /15 = ' . ($a / $b);
+echo '<br> 30 - 15 = ' . ($a - $b);
+echo '<br> 5 % 3 = ' . (5 % 3); 
+echo '<br> 2 ** 3 = ' . (2 ** 3); 
+
+define ('curso', 'AppWeb en Servidor');
+define ('password', 12345);
+
+echo '<br> Mi constante es: ' . (curso);
+echo '<br> La contraseña es: ' . (password);
+
+?>
